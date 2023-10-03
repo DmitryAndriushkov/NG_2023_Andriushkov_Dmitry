6b64238f2e2152da5e04b8d9d6829baab4348a0d
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2023-10-03 11:17:37 +0300

    Added second task of the first lection

---
+++ Lection_1/Task_2/main.py
@@ -0,0 +1,9 @@
+
+print("\nHi! This program will find the sum of two numbers")
+
+firstNum = float(input("Your first number: "))
+secondNum = float(input("Your second number: "))
+
+result = (firstNum + secondNum)
+
+print(f"{firstNum} + {secondNum} = {result}")
