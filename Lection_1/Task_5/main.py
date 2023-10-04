dc91fd0d08069d3dcc291afc5077ae1bd694db40
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2023-10-04 16:00:36 +0300

    Added solution to quadratic equations with negative discriminant

--- Lection_1/Task_5/main.py
+++ Lection_1/Task_5/main.py
@@ -1,7 +1,7 @@
 from math import sqrt
 
 print("\nHi! This program can solve quadratic equation by using Discriminant")
-print("ax² + bx + c\n\nD = b² - 4ac")
+print("ax² + bx + c = 0\n\nD = b² - 4ac")
 
 dA = float(input("\n'a' number: "))
 dB = float(input("'b' number: "))
@@ -9,13 +9,16 @@ dC = float(input("'c' number: "))
 Discriminant = ((dB**2) - 4 * dA * dC)
 
 if Discriminant > 0:
-    x1 = (-dB + sqrt(Discriminant)) / 2 * dA
-    x2 = (-dB - sqrt(Discriminant)) / 2 * dA
-    print(f"Roots of this quadratic equation are x1 = {x1}, x2 = {x2} ")
+    x1 = (-dB + sqrt(Discriminant)) / (2 * dA)
+    x2 = (-dB - sqrt(Discriminant)) / (2 * dA)
+    print(f"Roots of this quadratic equation are x1 = {x1}, x2 = {x2}")
 
 elif Discriminant == 0:
     x = (-dB) / 2 * dA
     print(f"There is just one root x = {x}")
 
-else:
-    print(f"Discriminant is less than 0, so there`s no roots\n")
+elif Discriminant < 0:
+    imaginariusNum = sqrt(abs(Discriminant))
+    x1I = (-dB + imaginariusNum) / (2 * dA)
+    x2I = (-dB - imaginariusNum) / (2 * dA)
+    print(f"Roots of negative Discriminant are x1 = {x1I} + i, x2 = {x2I} - i")
