bd127fea7f66ae0cb20a6305f3a12068420924b0
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2023-10-10 15:15:46 +0300

    Made my code simpler, changed list to a set

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,16 +1,12 @@
 
 print("\nHi! This simple program can output unique elements")
 
-userInput = int(input("Enter a value of elements you want: "))
+userInput = int(input("Enter the number of elements you want: "))
 
-userElems = []
-uniqueElems = []
+uniqueElems = set()
 
 for elems in range(userInput):
     strNum = input(f"[{elems + 1}] element: ")
-    userElems.append(strNum)
-    if strNum not in uniqueElems:
-        uniqueElems.append(strNum)
+    uniqueElems.add(strNum)
 
-print("Your elements: ", userElems)
-print("Unique elements: ", uniqueElems)
+print("\nUnique elements:", list(uniqueElems))
