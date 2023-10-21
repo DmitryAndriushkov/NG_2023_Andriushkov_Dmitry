85fe8dd6690f5ae7288219a00af8fc47e14c8c2e
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2023-10-21 21:11:09 +0300

    Made my code simpler

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,12 +1,2 @@
-
-print("\nHi! This simple program can output unique elements")
-
-userInput = int(input("Enter the number of elements you want: "))
-
-uniqueElems = set()
-
-for elems in range(userInput):
-    strNum = input(f"[{elems + 1}] element: ")
-    uniqueElems.add(strNum)
-
-print("\nUnique elements:", list(uniqueElems))
+uniqueElems = list(set(input("\nHi! This simple program can output unique elements\nEnter your string: ").split()))
+print("\nUnique elements:", sorted(list(uniqueElems)))
