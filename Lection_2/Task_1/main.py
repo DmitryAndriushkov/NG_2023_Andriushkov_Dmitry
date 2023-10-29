ef81622702c7a74436b9fbb5d9f0c40e085eb9ab
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2023-10-29 19:59:45 +0200

    Removed unnecessary function

--- Lection_2/Task_1/main.py
+++ Lection_2/Task_1/main.py
@@ -1,1 +1,1 @@
-print("Unique elements: ", sorted(list(set(input("\nHi! This simple program can output unique elements\nEnter your string: ").split()))))
+print("Unique elements: ", set(input("\nHi! This simple program can output unique elements\nEnter your string(separated by space): ").split()))
